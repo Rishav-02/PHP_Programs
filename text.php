377c41bd45ab8aacd0e60506a412b8f4ae4b3189
Rishav <rishavsinha380@gmail.com>
2023-01-14 11:29:09 +0530

    echo and concatenate string

---
+++ text.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>
+        PHP
+    </title>
+</head>
+
+<body>
+    <!-- echo is faster than print -->
+    <?php
+    echo'PHP INSIDE HTML';
+    echo "</br>";
+    echo "Rishav"." Raj";//concatenate string
+    echo "</br></br>";
+    echo nl2br("New line will start from here\n in this string\r\n on the browser window\n"); 
+    echo "A.S"," College";//comma give separate value
+    echo "<h4>India</h4>";
+    echo 22.10;
+    print "<h2>Deoghar</h2>";
+    // print "SKMU"," Dumka"; comma not allowed
+    ?>
+</body>
+
+</html>
