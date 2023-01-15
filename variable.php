1e15abf8f915e692a6c41103eda6750a52370602
Rishav <rishavsinha380@gmail.com>
2023-01-15 10:28:34 +0530

    variable in PHP

---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php
+$a = 'Ranchi<br>';
+$num = 234;
+$name = "Yahoo Baba<br>";
+$place="Hyderabad";
+echo $a;
+echo $num . "<br>";
+
+var_dump($num); //give type of datatype
+var_dump($place);
+?>
